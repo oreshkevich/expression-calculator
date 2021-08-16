db0793e73da72a8cc7f9b0e675f6c2840310f0c8
Henadzi <68184678+oreshkevich@users.noreply.github.com>
2021-08-16 19:38:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,28 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    function checkBalance(expr) {
+    let pack = [],
+      open = "(",
+      close = ")";
+    for (const symbol of expr) {
+      let i = open.indexOf(symbol);
+      if (i > -1) pack.push(close[i]);
+      if (close.includes(symbol) && symbol != pack.pop()) return false
+    }
+    return pack.length == 0;
+  }
+  if (checkBalance(expr) == true) {
+    let ff = expr;
+    let ggg = ff.replace(/\s/g, '');
+    let regexp = new RegExp(/\d*\/0/);
+    let isZeroDivision = regexp.test(ggg);
+    if (isZeroDivision) throw new RangeError("TypeError: Division by zero.");
+	return new Function(`return ${expr}`)();
+  } else {
+    throw new RangeError("ExpressionError: Brackets must be paired");
+  }
+	
 }
 
 module.exports = {
